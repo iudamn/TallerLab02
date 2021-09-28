879eba777bd90639cc9321d7dc97ea64045614c7
tonis <c.cartes07@ufromail.cl>
2021-09-28 09:07:19 -0300

    commit

--- src/main/java/metodos.java
+++ src/main/java/metodos.java
@@ -1,14 +1,25 @@
 public class metodos {
 
+    public static void menu(){
+        System.out.println("1.Mostrar pares adyacentes");
+        System.out.println("2.Mostrar el mayor valor de producto");
+        System.out.println("3.Salir");
+        elegirOpcion();
+    }
+
+    public static int elegirOpcion(){
+
+    }
+
     public static int elegirNumero(){
         int numero = (int) (Math.random() * 10) + 2;
-        generarArreglo();
+        generarArreglo(numero);
         return numero;
     }
 
-    public static int[] generarArreglo(){
-        for(){
-
+    public static int[] generarArreglo(int numero){
+        for(int i=0; i<numero; i++){
+            System.out.print("["+numero[i]+"]");
         }
     }
 }
